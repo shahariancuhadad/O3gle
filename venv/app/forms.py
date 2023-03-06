7ad9b34a1d71f8d3c9dd0841a7e12a84dd1f3415
shahariancuhadad <70977065+shahariancuhadad@users.noreply.github.com>
2023-03-06 14:11:56 +0200

    one comment

--- venv/app/forms.py
+++ venv/app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 from app.models import User
 
-class LoginForm(FlaskForm):
+class LoginForm(FlaskForm): #Login form definition as class with fields
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
