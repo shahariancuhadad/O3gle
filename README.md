1a98833351ebd581bc30727b669320449ba57f08
shahariancuhadad <70977065+shahariancuhadad@users.noreply.github.com>
2023-03-06 14:01:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# O3gle
+ Omegle style chat project with 2 and three participent options, made using python-flask framework and twilio api for video sharing and call management
---
+++ venv/app/models.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+from hashlib import md5
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class Screenshot(db.Model): #database class for storing list of screenshots taken by a specific user
+    id = db.Column(db.Integer, primary_key=True)
+    image_url = db.Column(db.String(64)) #screenshot url of location where it is stored
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #image saver / poster
+
+    def __repr__(self):
+        return f'<Screenshot {self.image_url}>'
+
+@login.user_loader
+def user_loader(id):
+    return User.query.get(int(id))
